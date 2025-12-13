004526808b57bf2c5f959d4dfd8187c3aa306255
tukaram40k <tukaram40k@gmail.com>
2025-12-13 20:12:25 +0200

    configure sinatra server

--- offer-calculator-service/main.rb
+++ offer-calculator-service/main.rb
@@ -1,7 +1,32 @@
 require 'bundler/setup'
 require 'sinatra'
+require 'sinatra/cross_origin'
+require 'dotenv'
 require_relative 'app/controllers/request_controller'
 require_relative 'app/models/config_data'
+
+configure do
+  enable :cross_origin
+end
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  response.headers['Access-Control-Allow-Methods'] = 'GET, PUT, OPTIONS'
+  response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With'
+  if request.content_type == 'application/json'
+    body = request.body.read
+    @json_params = JSON.parse(body) unless body.empty?
+  end
+end
+
+Dotenv.load
+PORT = ENV['PORT'].to_i
+
+abort 'invalid port. set it up in .env file' if PORT < 0
+
+set :port, PORT
+set :bind, '0.0.0.0'
+set :environment, :production
 
 get '/' do
   'Offer Calculator Service Up'
